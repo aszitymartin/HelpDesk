0073bb9d5b5511dda17e12710737c10c7768604e
aszitymartin <martinaszity@icloud.com>
2026-04-27 08:32:38 +0200

    Add Hungarian language support for sidenav

---
+++ lang/hu/sidenav.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    'dashboard' => "Dashboard",
+    'tickets'   => "Hibajegyek",
+    'events'    => "Események",
+
+
+    'categories' => "Kategóriák",
+    'labels'     => "Címkék",
+    'templates'  => "Sablonok",
+        'email'     => "Email",
+        'ticket'    => "Hibajegy",
+
+
+    'users'      => "Felhasználók",
+        'customers' => "Bejelentők",
+        'team'      => "Adminisztrátorok",
+
+    'modules'   => "Modulok",
+        'escalate'      => "Escalate",
+        'satisfaction'  => "Elégedettség",
+        'recurring'     => "Visszatérő hibajegyek",
+        'automations'   => "Automatizáció",
+
+
+    'reports'    => "Jelentések",
+    'statistics' => "Statisztika",
+    'files'      => "Csatlományok",
+
+
+    'tools'       => "Eszközök",
+        'service'    => "Rendszer üzenetek",
+        'fields'     => "Egyedi mezők",
+        'statuses'   => "Státuszok",
+        'priorities' => "Prioritások",
+
+    'settings'    => "Beállítésok",
+        'general'   => "Általános",
+        'helpdesk'  => "HelpDesk",
+        'looks'     => "Kinézet & Stílus",
+        'email'     => "Email",
+        'misc'      => "Egyéb",
+        
+
+];
