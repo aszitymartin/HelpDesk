b235e116e118013be0ca348ce7cc2779be85ce9a
aszitymartin <martinaszity@icloud.com>
2026-04-28 07:29:40 +0200

    Enhance header styling, update profile link, and improve form input classes for better UX

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -20,7 +20,7 @@
 
             <el-menu anchor="bottom end" popover class="w-56 origin-top-right divide-y divide-white/10 rounded-md bg-gray-800 outline-1 -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
                 <div class="py-1">
-                    <a href="#" class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden">View Profile</a>
+                    <a href="/profile" class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden">View Profile</a>
                     <a href="#" class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden">Duplicate</a>
                 </div>
                 <div class="py-1">
