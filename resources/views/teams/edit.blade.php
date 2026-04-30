80df06537cc4e7c1e90d773d889fb94579ef5297
aszitymartin <martinaszity@icloud.com>
2026-04-30 13:12:09 +0200

    Enhance team management UI by adding disabled state for system teams; update edit and delete actions accordingly

--- resources/views/teams/edit.blade.php
+++ resources/views/teams/edit.blade.php
@@ -1,11 +1,15 @@
 <x-app-layout>
-    <form class="flex flex-col gap-4 item-bg text-white p-4" method="POST" action="{{ route('teams.update', $team) }}">
-        @csrf
-        @method('PUT')
-        <div class="flex flex-col gap-1">
-            <label for="team_name">{{ __('teams.Team name') }}</label>
-            <input class="innerp" type="text" id="team_name" name="name" value="{{ $team->name }}" placeholder="{{ __('teams.Team name') }}">
-        </div>
-        <button class="button w-fc" type="submit">{{ __('general.update') }}</button>
-    </form>
+    @if($team->is_system === false)
+        <form class="flex flex-col gap-4 item-bg text-white p-4" method="POST" action="{{ route('teams.update', $team) }}">
+            @csrf
+            @method('PUT')
+            <div class="flex flex-col gap-1">
+                <label for="team_name">{{ __('teams.Team name') }}</label>
+                <input class="innerp" type="text" id="team_name" name="name" value="{{ $team->name }}" placeholder="{{ __('teams.Team name') }}">
+            </div>
+            <button class="button w-fc" type="submit">{{ __('general.update') }}</button>
+        </form>
+    @else
+        {{ abort('403') }}
+    @endif
 </x-app-layout>
