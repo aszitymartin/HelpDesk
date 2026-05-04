ca7e6ba6ce4c382641c602098547ced9dc34cc47
aszitymartin <martinaszity@icloud.com>
2026-05-04 09:05:04 +0200

    Update role, team, and user management UIs to conditionally display edit and delete actions based on system status

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -15,7 +15,7 @@
 
                     <div class="flex flex-row items-center gap-4">
 
-                        @if($team->is_system === false)
+                        @if($team->is_system === 0)
                             <a class="flex flex-row items-center gap-1 button" href="{{ route('teams.edit', $team) }}">
                                 <svg class="text-white w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#FFF" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>
                                 <span>{{ __('general.edit') }}</span>
@@ -28,7 +28,7 @@
                                 <button type="submit">{{ __('general.delete') }}</button>
                             </form>
                         @else
-                            <span class="badge whitespace-nowrap mb-2 inline-block text-sm bg-slate-800 hover:bg-slate-700 text-white border border-slate-700 py-1 px-3 rounded transition-all">
+                            <span class="badge whitespace-nowrap mb-2 inline-block cursor-not-allowed text-sm bg-slate-800 text-white border border-slate-700 py-1 px-3 rounded transition-all">
                                 <i class="fa-solid fa-lock"></i> Locked
                             </span>
                         @endif
